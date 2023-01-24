e7f3d698ca53dcf63e305a16e2662bcf23984801
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-01-24 12:03:12 +0100

    add home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import './App.css'
+import Navbar from "./Navbar"
+import SocialLinks from "./SocialLinks"
 
 function App() {
-  return (
-      <div>
-      </div>
-  );
+    return (
+        <div>
+            <header id="header">
+                < Navbar/>
+            </header>
+            <div id={"home-container"}>
+                <div id={"hello"}><h1>Hi there,</h1></div>
+                <div id={"introduction-1"}><h2>My name is <span className={"text-highlight"}>Marcin Sobczyk</span></h2></div>
+                <div id={"introduction-2"}><h3>I am self taught Front-End Developer</h3></div>
+            </div>
+            <footer className={"home-footer"}>
+            <SocialLinks/>
+            </footer>
+        </div>
+    )
 }
 
-export default App;
+export default App
