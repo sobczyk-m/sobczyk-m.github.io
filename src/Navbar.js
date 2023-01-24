6eaa1dd08a64bf1ebc9cd45aad9d39e61cc3e7c0
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-01-24 11:36:57 +0100

    add Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav className={"navbar"}>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
