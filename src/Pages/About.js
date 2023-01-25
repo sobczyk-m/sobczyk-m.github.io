8c1febaa532e0678fe16488f7407206780af6e55
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-01-25 10:53:25 +0100

    add About page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,8 +1,30 @@
+import authorPhoto from "../author-photo.jpg"
+import TechStack from "../Components/TechStack"
 
 function About() {
     return (
         <div>
-
+            <div className={"wide-wrapper"}>
+                <div className={"message-container"}>
+                    <h1>{"< "}Hello World{" />"}</h1>
+                    <span className={"text-highlight"}></span>
+                    <p>
+                        Hi! I'm Marcin. Just some random guy that loves technology, solving problems and creating stuff.
+                        One day I combined these three factors together and found myself on the programmer path.
+                        {"\n"}Currently, I concentrate my efforts on learning the best practices of Web Development, and
+                        I spend
+                        my spare time working on personal projects in this field.
+                    </p>
+                    <p>
+                        What’s more about me. I'm a well-organized and self-disciplined person.
+                        I enjoy working as a team and I'm not afraid of working on independent tasks.
+                    </p>
+                    <p>I'm looking for professional opportunities in developer positions that will allow me to work on
+                        interesting Front-End projects.</p>
+                    <p>My Tech Stack:</p>
+                </div>
+                <img className={"image"} src={authorPhoto} height={"500px"} alt="Photo of website author"/>
+            </div>
         </div>
     )
 }
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -3,7 +3,7 @@ import SocialLinks from "../Components/SocialLinks"
 function Contact() {
     return (
         <div>
-            <div className={"wrapper"}>
+            <div className={"short-wrapper"}>
                 <h1>Let's Get In Touch</h1>
                 <p><span className={"text-highlight"}>I'm looking for new job opportunities</span>. Hit me up
                     if you have some questions or want to work together. I'll try to get back to you as soon as I can.
