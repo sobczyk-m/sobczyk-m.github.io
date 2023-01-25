4b7a354adf27b2b23f7dcbb5126ec6140be3be9c
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-01-25 15:31:14 +0100

    add popup name to TechStack component

--- src/Components/TechStack.js
+++ src/Components/TechStack.js
@@ -3,15 +3,24 @@ function TechStack() {
     return (
         <div className={"tech-stack"}>
             <ul>
-                <li><i className="fa-brands fa-react"></i></li>
-                <li><i className="fa-brands fa-square-js"></i></li>
-                <li><i className="fa-brands fa-git-alt"></i></li>
-                <li><i className="fa-brands fa-html5"></i></li>
-                <li><i className="fa-brands fa-css3-alt"></i></li>
+                <li><i className="fa-brands fa-react">
+                    <div className={"tech-name-container"}><p className={"tech-name"}>React</p></div>
+                </i></li>
+                <li><i className="fa-brands fa-square-js">
+                    <div className={"tech-name-container"}><p className={"tech-name"}>JavaScript</p></div>
+                </i></li>
+                <li><i className="fa-brands fa-git-alt">
+                    <div className={"tech-name-container"}><p className={"tech-name"}>Git</p></div>
+                </i></li>
+                <li><i className="fa-brands fa-html5">
+                    <div className={"tech-name-container"}><p className={"tech-name"}>HTML</p></div>
+                </i></li>
+                <li><i className="fa-brands fa-css3-alt">
+                    <div className={"tech-name-container"}><p className={"tech-name"}>CSS</p></div>
+                </i></li>
             </ul>
         </div>
     )
-
 }
 
 export default TechStack
