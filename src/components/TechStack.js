fb7a2856429e16089527d4ff0ffbb344ca1f0c06
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-03-07 14:37:46 +0100

    export TechStack component css code to separate techStack.scss

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -3,19 +3,19 @@ function TechStack(props) {
     return (
         <div className={"tech-stack"} onAnimationEnd={event => props.onAnimationEnd(event)}>
             <ul>
-                <li><i className="fa-brands fa-react">
+                <li><i className="tech-icon fa-brands fa-react">
                     <div className={"tech-name-container"}><p className={"tech-name"}>React</p></div>
                 </i></li>
-                <li><i className="fa-brands fa-square-js">
+                <li><i className="tech-icon fa-brands fa-square-js">
                     <div className={"tech-name-container"}><p className={"tech-name"}>JavaScript</p></div>
                 </i></li>
-                <li><i className="fa-brands fa-git-alt">
+                <li><i className="tech-icon fa-brands fa-git-alt">
                     <div className={"tech-name-container"}><p className={"tech-name"}>Git</p></div>
                 </i></li>
-                <li><i className="fa-brands fa-html5">
+                <li><i className="tech-icon fa-brands fa-html5">
                     <div className={"tech-name-container"}><p className={"tech-name"}>HTML</p></div>
                 </i></li>
-                <li><i className="fa-brands fa-css3-alt">
+                <li><i className="tech-icon fa-brands fa-css3-alt">
                     <div className={"tech-name-container"}><p className={"tech-name"}>CSS</p></div>
                 </i></li>
             </ul>
