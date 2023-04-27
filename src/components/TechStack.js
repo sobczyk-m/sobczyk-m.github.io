9b44acb6b08866a8af388291a9b370c5a40ebf74
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-04-27 21:42:10 +0200

    add java to tech stack and update author's photo

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -3,14 +3,14 @@ function TechStack(props) {
     return (
         <div className={"tech-stack"} onAnimationEnd={event => props.onAnimationEnd(event)}>
             <ul>
-                <li><i className="tech-icon fa-brands fa-react">
-                    <div className={"tech-name-container"}><p className={"tech-name"}>React</p></div>
+                <li><i className="tech-icon fa-brands fa-java">
+                    <div className={"tech-name-container"}><p className={"tech-name"}>Java</p></div>
                 </i></li>
                 <li><i className="tech-icon fa-brands fa-square-js">
                     <div className={"tech-name-container"}><p className={"tech-name"}>JavaScript</p></div>
                 </i></li>
-                <li><i className="tech-icon fa-brands fa-git-alt">
-                    <div className={"tech-name-container"}><p className={"tech-name"}>Git</p></div>
+                <li><i className="tech-icon fa-brands fa-react">
+                    <div className={"tech-name-container"}><p className={"tech-name"}>React</p></div>
                 </i></li>
                 <li><i className="tech-icon fa-brands fa-sass">
                     <div className={"tech-name-container"}><p className={"tech-name"}>CSS</p></div>
@@ -20,6 +20,9 @@ function TechStack(props) {
                 </i></li>
                 <li><i className="tech-icon fa-brands fa-html5">
                     <div className={"tech-name-container"}><p className={"tech-name"}>HTML</p></div>
+                </i></li>
+                <li><i className="tech-icon fa-brands fa-git-alt">
+                    <div className={"tech-name-container"}><p className={"tech-name"}>Git</p></div>
                 </i></li>
             </ul>
         </div>
