b8c336ddcc16e5203749ca3e334c35616a2591a9
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-08-19 13:53:06 +0200

    hide some old projects

--- src/data/projectsCollection.js
+++ src/data/projectsCollection.js
@@ -1,6 +1,6 @@
-import quoteMachine from "../assets/images/Quote-Machine.png"
-import markdownPreviewer from "../assets/images/Markdown-Previewer.png"
-import drumMachine from "../assets/images/Drum-Machine.png"
+// import quoteMachine from "../assets/images/Quote-Machine.png"
+// import markdownPreviewer from "../assets/images/Markdown-Previewer.png"
+// import drumMachine from "../assets/images/Drum-Machine.png"
 import calculator from "../assets/images/Calculator.png"
 import weatherApp from "../assets/images/Weather-App.png"
 import personalWebsite from "../assets/images/Personal-Website.png"
@@ -30,34 +30,34 @@ const projects = [
         imageLink: personalWebsite,
         githubLink: "https://github.com/sobczyk-m/sobczyk-m.github.io",
         playgroundLink: "https://sobczyk-m.github.io/"
-    },
-    {
-        name: "Drum Machine",
-        description: "Use the drum machine to produce your own tunes.",
-        techStack: ["React", "JavaScript", "HTML", "CSS"],
-        imageLink: drumMachine,
-        githubLink: "https://github.com/sobczyk-m/Drum-Machine",
-        codeLink: "https://codesandbox.io/s/vx5pzu",
-        playgroundLink: "https://vx5pzu.csb.app/"
-    },
-    {
-        name: "Quote Machine",
-        description: "Get your life inspired by some random quotes.",
-        techStack: ["React", "JavaScript", "jQuery", "HTML", "CSS"],
-        imageLink: quoteMachine,
-        githubLink: "https://github.com/sobczyk-m/Quote-Machine",
-        codeLink: "https://codesandbox.io/s/8h0wjb",
-        playgroundLink: "https://8h0wjb.csb.app/"
-    },
-    {
-        name: "Markdown Previewer",
-        description: "Check out your writing once you've added the markdown syntax.",
-        techStack: ["React", "JavaScript", "HTML", "CSS"],
-        imageLink: markdownPreviewer,
-        githubLink: "https://github.com/sobczyk-m/Markdown-Previewer",
-        codeLink: "https://codesandbox.io/s/zd9r94",
-        playgroundLink: "https://zd9r94.csb.app/"
     }
+    // {
+    //     name: "Drum Machine",
+    //     description: "Use the drum machine to produce your own tunes.",
+    //     techStack: ["React", "JavaScript", "HTML", "CSS"],
+    //     imageLink: drumMachine,
+    //     githubLink: "https://github.com/sobczyk-m/Drum-Machine",
+    //     codeLink: "https://codesandbox.io/s/vx5pzu",
+    //     playgroundLink: "https://vx5pzu.csb.app/"
+    // },
+    // {
+    //     name: "Quote Machine",
+    //     description: "Get your life inspired by some random quotes.",
+    //     techStack: ["React", "JavaScript", "jQuery", "HTML", "CSS"],
+    //     imageLink: quoteMachine,
+    //     githubLink: "https://github.com/sobczyk-m/Quote-Machine",
+    //     codeLink: "https://codesandbox.io/s/8h0wjb",
+    //     playgroundLink: "https://8h0wjb.csb.app/"
+    // },
+    // {
+    //     name: "Markdown Previewer",
+    //     description: "Check out your writing once you've added the markdown syntax.",
+    //     techStack: ["React", "JavaScript", "HTML", "CSS"],
+    //     imageLink: markdownPreviewer,
+    //     githubLink: "https://github.com/sobczyk-m/Markdown-Previewer",
+    //     codeLink: "https://codesandbox.io/s/zd9r94",
+    //     playgroundLink: "https://zd9r94.csb.app/"
+    // }
 ]
 
 export default projects
