a1fc6ee7f809db6e084a0d37682f8f1ca3f2efc8
Marcin Sobczyk <marcin.sobczyk.dev@gmail.com>
2023-12-19 13:49:59 +0100

    update tech stack

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,5 +1,5 @@
 import {FaCss3Alt, FaGitAlt, FaHtml5, FaJava, FaReact} from "react-icons/fa";
-import {SiKotlin, SiSpringboot} from "react-icons/si";
+import {SiJetbrains, SiKotlin, SiPostman, SiPython, SiSpringboot} from "react-icons/si";
 import {IoLogoJavascript} from "react-icons/io5";
 import {IoLogoSass} from "react-icons/io";
 import {TbSql} from "react-icons/tb";
@@ -19,20 +19,20 @@ function TechStack(props) {
                     <div className={"tech-name-container"}><p className={"tech-name"}>Kotlin</p></div>
                 </li>
                 <li className={"tech-icon"}>
-                    <TfiAndroid/>
-                    <div className={"tech-name-container"}><p className={"tech-name"}>Android</p></div>
+                    <SiPython/>
+                    <div className={"tech-name-container"}><p className={"tech-name"}>Python</p></div>
                 </li>
                 <li className={"tech-icon"}>
                     <IoLogoJavascript/>
                     <div className={"tech-name-container"}><p className={"tech-name"}>JavaScript</p></div>
                 </li>
                 <li className={"tech-icon"}>
-                    <FaGitAlt/>
-                    <div className={"tech-name-container"}><p className={"tech-name"}>Git</p></div>
-                </li>
-                <li className={"tech-icon"}>
                     <TbSql/>
                     <div className={"tech-name-container"}><p className={"tech-name"}>SQL</p></div>
+                </li>
+                <li className={"tech-icon"}>
+                    <TfiAndroid/>
+                    <div className={"tech-name-container"}><p className={"tech-name"}>Android</p></div>
                 </li>
                 <li className={"tech-icon"}>
                     <SiSpringboot/>
@@ -43,8 +43,12 @@ function TechStack(props) {
                     <div className={"tech-name-container"}><p className={"tech-name"}>React</p></div>
                 </li>
                 <li className={"tech-icon"}>
-                    <IoLogoSass/>
-                    <div className={"tech-name-container"}><p className={"tech-name"}>CSS</p></div>
+                    <FaGitAlt/>
+                    <div className={"tech-name-container"}><p className={"tech-name"}>Git</p></div>
+                </li>
+                <li className={"tech-icon"}>
+                    <SiPostman/>
+                    <div className={"tech-name-container"}><p className={"tech-name"}>Postman</p></div>
                 </li>
                 <li className={"tech-icon"}>
                     <FaCss3Alt/>
@@ -53,6 +57,14 @@ function TechStack(props) {
                 <li className={"tech-icon"}>
                     <FaHtml5/>
                     <div className={"tech-name-container"}><p className={"tech-name"}>HTML</p></div>
+                </li>
+                <li className={"tech-icon"}>
+                    <IoLogoSass/>
+                    <div className={"tech-name-container"}><p className={"tech-name"}>Sass</p></div>
+                </li>
+                <li className={"tech-icon"}>
+                    <SiJetbrains/>
+                    <div className={"tech-name-container"}><p className={"tech-name"}>JetBrains IDEs</p></div>
                 </li>
             </ul>
         </div>
